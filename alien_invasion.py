44f8de82a2dc54a1318cb55aa6e0b02f37c6fb57
sbarcoe90 <sbarcoe@outlook.ie>
2024-07-08 15:40:58 +0100

    Add ship and draw it to screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behaviour."""
@@ -17,6 +18,8 @@ class AlienInvasion:
 
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
         #Set the background color
         self.bg_color = (230, 230, 230)
 
@@ -30,6 +33,7 @@ class AlienInvasion:
 
             #Redraw the screen during eaech pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
