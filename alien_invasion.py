9dc75e8160ca89b2429a51c0961ca7e0ed5b54b9
sbarcoe90 <sbarcoe@outlook.ie>
2024-07-08 20:24:48 +0100

    Added continuous movement to right key

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,6 +28,7 @@ class AlienInvasion:
         while True:
             # Watch for keyboard and mouse events.
             self._check_events()
+            self.ship.update()
             self._update_screen()            
             self.clock.tick(60)
 
@@ -38,8 +39,10 @@ class AlienInvasion:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RIGHT:
-                        # Move the ship to the right.
-                        self.ship.rect.x += 1
+                        self.ship.moving_right = True
+                elif event.type == pygame.KEYUP:
+                        if event.key == pygame.K_RIGHT:
+                            self.ship.moving_right = False
     
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
