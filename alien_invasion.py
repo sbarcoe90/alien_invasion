6303499f96040368d161d05ec4fc81ea80287c9a
sbarcoe90 <sbarcoe@outlook.ie>
2024-07-08 14:46:01 +0100

    Created Pygame window!

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,2 +1,28 @@
 import sys
-import pygamr
+import pygame
+
+class AlienInvasion:
+    """Overall class to manage game assets and behaviour."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources"""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Start the main loop for the game"""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
