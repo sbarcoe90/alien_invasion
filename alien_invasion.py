a68fa2a286271328d77b1f1d7d728acb50b28d1d
sbarcoe90 <sbarcoe@outlook.ie>
2024-07-09 13:44:09 +0100

    Tidied up main class

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,7 +19,6 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height)
         )
-
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
@@ -48,7 +47,6 @@ class AlienInvasion:
                 elif event.type == pygame.KEYUP:
                     self._check_keyup_events(event)
                         
-
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
         if event.key == pygame.K_RIGHT:
