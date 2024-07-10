39a62fba63872a9b5345303cd07bc5481065c45a
sbarcoe90 <sbarcoe@outlook.ie>
2024-07-10 10:34:51 +0100

    Built Alien Fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -88,9 +88,19 @@ class AlienInvasion:
     
     def _create_fleet(self):
         """Create the fleet of aliens."""
-        # Make an alien.
+        # Create an alien and keep adding aliens until there's no room left.
+        # Spacing between aliens is one alien width.
+
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+
+        current_x = alien_width
+        while current_x < (self.settings.screen_width - 2 * alien_width):
+            new_alien = Alien(self)
+            new_alien.x = current_x
+            new_alien.rect.x = current_x
+            self.aliens.add(new_alien)
+            current_x += 2 * alien_width
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
@@ -99,8 +109,6 @@ class AlienInvasion:
             bullet.draw_bullet()
         self.ship.blitme()
         self.aliens.draw(self.screen)
-
-
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
