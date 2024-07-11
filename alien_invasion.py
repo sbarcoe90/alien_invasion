ca9335cd4fd990da72fca62907582fb30c18133e
sbarcoe90 <sbarcoe@outlook.ie>
2024-07-11 10:31:07 +0100

    Aliens now move to the right

--- alien_invasion.py
+++ alien_invasion.py
@@ -37,7 +37,8 @@ class AlienInvasion:
             # Watch for keyboard and mouse events.
             self._check_events()
             self.ship.update()
-            self._update_bullets()   
+            self._update_bullets()
+            self._update_aliens()   
             self._update_screen()            
             self.clock.tick(60)
 
@@ -85,6 +86,9 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
                 # print(len(self.bullets))
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
     
     def _create_fleet(self):
         """Create the fleet of aliens."""
--- settings.py
+++ settings.py
@@ -17,3 +17,7 @@ class Settings:
         self.bullet_height = 6
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+
+        # Alien Settings
+        self.alien_speed = 1.0
+        
