06ca6c1c53a19b93d0579e44aa8700f9808689bd
sbarcoe90 <sbarcoe@outlook.ie>
2024-07-11 11:28:31 +0100

    Repopulated Fleet after old fleet is destroyed

--- alien_invasion.py
+++ alien_invasion.py
@@ -84,6 +84,11 @@ class AlienInvasion:
         # If so, get rid of the bullet and the alien.
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
+
         # Get rid of bullets that have disappeared.
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
