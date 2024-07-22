7fca462e2f3653111c2f87853b5a7fc3dea69d15
sbarcoe90 <sbarcoe@outlook.ie>
2024-07-22 18:10:34 +0100

    Deactivated Play Button from Screen after it is clicked

--- alien_invasion.py
+++ alien_invasion.py
@@ -70,7 +70,8 @@ class AlienInvasion:
 
     def _check_play_button(self, mouse_pos):
         """Start a new game when the player clicks Play."""
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.game_active:
             # Reset the game statistics.
             self.stats.reset_stats()
             self.game_active = True
