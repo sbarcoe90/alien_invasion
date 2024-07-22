466a80fe5a77a19342c278ff94e15810150f8f7d
sbarcoe90 <sbarcoe@outlook.ie>
2024-07-22 18:58:09 +0100

    Added increased difficulty by speeding up Aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -72,6 +72,7 @@ class AlienInvasion:
         """Start a new game when the player clicks Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.game_active:
+            self.settings.initialize_dynamic_settings()
             self.start_game()
 
     def start_game(self):
@@ -139,6 +140,7 @@ class AlienInvasion:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _update_aliens(self):
         """Update the positions of all aliens in the fleet."""
--- settings.py
+++ settings.py
@@ -9,18 +9,35 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship Settings
-        self.ship_speed = 8.5
-        self.ship_limit = 1
+        # self.ship_speed = 30.0
+        self.ship_limit = 2
 
         # Bullet Settings
-        self.bullet_speed = 6.0
-        self.bullet_width = 800
-        self.bullet_height = 6
+        self.bullet_width = 3
+        self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 40
 
         # Alien Settings
-        self.alien_speed = 20.0
-        self.fleet_drop_speed = 20.0
+        # self.alien_speed = 20.0
+        self.fleet_drop_speed = 40
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        self.ship_speed = 1.5
+        self.bullet_speed = 2.5
+        self.alien_speed = 1.0
+
         # fleet_deirection of 1 represents right; -1 represents left.
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        """ Increase speed settings."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
